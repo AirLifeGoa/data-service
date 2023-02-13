5683ca2e083afb30a5506fdbf0478478f20df941
Pranav <pranavbalijapelly@gmail.com>
2023-02-13 23:56:39 +0530

    fix: change in data loading pipeline with filters

--- src/routes/get-pollution-data-with-filters.ts
+++ src/routes/get-pollution-data-with-filters.ts
@@ -8,71 +8,293 @@ import { BadRequestError, currentUser, requireAuth } from '@airlifegoa/common';
 const router = express.Router();
 
 const QueryList = {
-  max: {
-    $set: {
-      PM10Data: {
-        $sortArray: {
-          input: '$PM10Data',
-          sortBy: -1,
+    "avg_pipeline": [
+        {
+            $unwind: '$data',
         },
-      },
-      NO2Data: {
-        $sortArray: {
-          input: '$NO2Data',
-          sortBy: -1,
+        {
+            $project: {
+                "data.data.PM10": { "$ifNull": [ "$data.data.PM10", 0 ] },
+                "data.data.SO2": { "$ifNull": [ "$data.data.SO2", 0 ] },
+                "data.data.NO2": { "$ifNull": [ "$data.data.NO2", 0 ] },
+                "data.data.Pb": { "$ifNull": [ "$data.data.Pb", 0 ] },
+                "data.data.O3": { "$ifNull": [ "$data.data.O3", 0 ] },
+                "data.data.PM25": { "$ifNull": [ "$data.data.PM2.5", 0 ] },
+                "_id": 1,
+                "data.recordedAt": "$data.recordedAt",
+                "data.metadata" : "$data.metadata",
+                "data.uploadedBy": "$data.uploadedBy",
+                "data._id": "$data._id",
+            }
         },
-      },
-      SO2Data: {
-        $sortArray: {
-          input: '$SO2Data',
-          sortBy: -1,
+        {
+            $group: {
+                _id: "$_id",
+                "avgPM10": {$avg : "$data.data.PM10"},
+                "avgSO2": {$avg : "$data.data.SO2"},
+                "avgNO2": {$avg : "$data.data.NO2"},
+                "avgO3": {$avg : "$data.data.O3"},
+                "avgPM25": {$avg : "$data.data.PM25"},
+                "avgPb": {$avg : "$data.data.Pb"},
+                "data": {$first: "$$ROOT"}
+              }
         },
-      },
-      O3Data: {
-        $sortArray: {
-          input: '$o3',
-          sortBy: -1,
+        {
+            $sort: {
+              _id: 1,
+            },
         },
-      },
-    },
-  },
-  min: {
-    $set: {
-      PM10Data: {
-        $sortArray: {
-          input: '$PM10Data',
-          sortBy: 1,
+        {
+            $project:{
+                "_id": 1,
+                "data.recordedAt": "$data.data.recordedAt",
+                "data.metadata" : "$data.data.metadata",
+                "data.data": "$data.data.data",
+                "data.uploadedBy": "$data.data.uploadedBy",
+                "data._id": "$data.data._id",
+                "avgPM10": 1,
+                "avgSO2":1,
+                "avgNO2": 1,
+                "avgO3": 1,
+                "avgPM25":1,
+                "avgPb":1
+              }
         },
-      },
-      NO2Data: {
-        $sortArray: {
-          input: '$NO2Data',
-          sortBy: 1,
+        {
+            $set: {
+              "data.data.PM10" : "$avgPM10",
+              "data.data.SO2" : "$avgSO2",
+              "data.data.Pb" : "$avgPb",
+              "data.data.PM25" : "$avgPM25",
+              "data.data.O3" : "$avgO3",
+              "data.data.NO2" : "$avgNO2",
+            }
         },
-      },
-      SO2Data: {
-        $sortArray: {
-          input: '$SO2Data',
-          sortBy: 1,
+        {
+            $project: {
+                "avgPM10":0,
+                "avgPM25":0,
+                "avgNO2":0,
+                "avgSO2":0,
+                "avgO3":0,
+                "avgPb":0
+            },
         },
-      },
-      O3Data: {
-        $sortArray: {
-          input: '$o3',
-          sortBy: 1,
+        {
+            $replaceRoot: {
+              newRoot: '$data',
+            },
         },
-      },
-    },
-  },
-
-  avg: {
-    $project: {
-      PM10Data: { $avg: '$PM10Data' },
-      NO2Data: { $avg: '$NO2Data' },
-      SO2Data: { $avg: '$SO2Data' },
-      O3Data: { $avg: '$O3Data' },
-    },
-  },
+        {
+            $project: {
+              _id: 0,
+              id: '$_id',
+              timestamp: 1,
+              data: 1,
+              metadata: 1,
+              recordedAt: 1,
+              uploadedBy: 1,
+            },
+        }
+    ],
+    "min_pipeline":[
+        {
+            $project: {
+                "data.data.PM10": { "$ifNull": [ "$data.data.PM10", "null" ] },
+                "data.data.SO2": { "$ifNull": [ "$data.data.SO2", "null" ] },
+                "data.data.NO2": { "$ifNull": [ "$data.data.NO2", "null" ] },
+                "data.data.Pb": { "$ifNull": [ "$data.data.Pb", "null" ] },
+                "data.data.O3": { "$ifNull": [ "$data.data.O3", "null" ] },
+                "data.data.PM25": { "$ifNull": [ "$data.data.PM2.5", "null" ] },
+                "_id": 1,
+                "data.recordedAt": "$data.recordedAt",
+                "data.metadata" : "$data.metadata",
+                "data.uploadedBy": "$data.uploadedBy",
+                "data._id": "$data._id",
+           }
+        },
+        {
+            $project:{
+                data: {
+                    "$arrayElemAt": [ "$data", 0 ]
+                }
+            }
+        },
+        {
+            $set: {
+                "data.recordedAt": {
+                    "$arrayElemAt": [ "$data.recordedAt", 0 ]
+                 },
+                "data.metadata": {
+                    "$arrayElemAt": [ "$data.metadata", 0 ]
+                },
+                "data.uploadedBy": {
+                    "$arrayElemAt": [ "$data.uploadedBy", 0 ]
+                },
+                "data._id":{
+                  "$arrayElemAt": ["$data._id",0]
+                }
+              }
+        },
+        {
+            $set: {
+                "minPM10": {
+                  $min:"$data.data.PM10"
+                },
+                "minSO2": {
+                  $min:"$data.data.SO2"
+                },
+                "minNO2": {
+                  $min:"$data.data.NO2"
+                }
+                ,
+                "minO3": {
+                  $min:"$data.data.O3"
+                },
+                "minPb": {
+                  $min:"$data.data.Pb"
+                },
+                "minPM25": {
+                  $min:"$data.data.PM2.5"
+                },
+              }  
+        },
+        {
+            $set: {
+                "data.data.PM10" : "$minPM10",
+                "data.data.SO2" : "$minSO2",
+                "data.data.Pb" : "$minPb",
+                "data.data.PM25" : "$minPM25",
+                "data.data.O3" : "$minO3",
+                "data.data.NO2" : "$minNO2",
+              }
+        },
+        {
+            $project:{
+                "minPM10":0,
+                "minSO2":0,
+                "minPb":0,
+                "minPM25":0,
+                "minO3":0,
+                "minNO2":0
+            }
+        },
+        {
+            $replaceRoot: {
+              newRoot: '$data',
+            },
+        },
+        {
+            $project: {
+              _id: 0,
+              id: '$_id',
+              timestamp: 1,
+              data: 1,
+              metadata: 1,
+              recordedAt: 1,
+              uploadedBy: 1,
+            },
+        }
+    ],
+    "max_pipeline":[
+        {
+            $project: {
+                "data.data.PM10": { "$ifNull": [ "$data.data.PM10", "null" ] },
+                "data.data.SO2": { "$ifNull": [ "$data.data.SO2", "null" ] },
+                "data.data.NO2": { "$ifNull": [ "$data.data.NO2", "null" ] },
+                "data.data.Pb": { "$ifNull": [ "$data.data.Pb", "null" ] },
+                "data.data.O3": { "$ifNull": [ "$data.data.O3", "null" ] },
+                "data.data.PM25": { "$ifNull": [ "$data.data.PM2.5", "null" ] },
+                "_id": 1,
+                "data.recordedAt": "$data.recordedAt",
+                "data.metadata" : "$data.metadata",
+                "data.uploadedBy": "$data.uploadedBy",
+                "data._id": "$data._id",
+           }
+        },
+        {
+            $project:{
+                data: {
+                    "$arrayElemAt": [ "$data", 0 ]
+                }
+            }
+        },
+        {
+            $set: {
+                "data.recordedAt": {
+                    "$arrayElemAt": [ "$data.recordedAt", 0 ]
+                 },
+                "data.metadata": {
+                    "$arrayElemAt": [ "$data.metadata", 0 ]
+                },
+                "data.uploadedBy": {
+                    "$arrayElemAt": [ "$data.uploadedBy", 0 ]
+                },
+                "data._id":{
+                  "$arrayElemAt": ["$data._id",0]
+                }
+              }
+        },
+        {
+            $set: {
+                "maxPM10": {
+                  $max:"$data.data.PM10"
+                },
+                "maxSO2": {
+                  $max:"$data.data.SO2"
+                },
+                "maxNO2": {
+                  $max:"$data.data.NO2"
+                }
+                ,
+                "maxO3": {
+                  $max:"$data.data.O3"
+                },
+                "maxPb": {
+                  $max:"$data.data.Pb"
+                },
+                "maxPM25": {
+                  $max:"$data.data.PM2.5"
+                },
+            }
+        },
+        {
+            $set: {
+                "data.data.PM10" : "$maxPM10",
+                "data.data.SO2" : "$maxSO2",
+                "data.data.Pb" : "$maxPb",
+                "data.data.PM25" : "$maxPM25",
+                "data.data.O3" : "$maxO3",
+                "data.data.NO2" : "$maxNO2",
+            }
+        },
+        {
+            $project: {
+                "maxPM10":0,
+                "maxSO2":0,
+                "maxPb":0,
+                "maxPM25":0,
+                "maxO3":0,
+                "maxNO2":0
+            }
+        },
+        {
+            $replaceRoot: {
+              newRoot: '$data',
+            },
+        },
+        {
+            $project: {
+              _id: 0,
+              id: '$_id',
+              timestamp: 1,
+              data: 1,
+              metadata: 1,
+              recordedAt: 1,
+              uploadedBy: 1,
+            },
+        }
+    ]
 };
 
 // url should contain optional query params pageNumber and pageSize
@@ -114,7 +336,7 @@ router.post(
     const endDate = new Date(req.body.endDate);
     var filterValue = req.body.filter;
     const query = req.body.stats;
-    const metric = req.body.metric;
+
 
     if (filterValue == 'weekly') {
       filterValue = { $week: '$recordedAt' };
@@ -123,9 +345,8 @@ router.post(
     } else if (filterValue == 'yearly') {
       filterValue = { $year: '$recordedAt' };
     }
-    const metricpipeline = "data.data."+metric;
-
-    console.log(filterValue, metricpipeline);
+    
+    console.log(filterValue);
     // get pollution data from database
     // filter duplicate data based on meta.addedAt and select one which is latest
     // use aggregation pipeline
@@ -134,7 +355,7 @@ router.post(
     // add id field which is _id field
     console.log('id ', typeof req.params.dataSourceId);
     console.log(startDate, endDate, filterValue);
-    const pipeline: any[] = [
+    var pipeline: any[] = [
       {
         $match: {
           'metadata.dataSourceId': req.params.dataSourceId,
@@ -180,87 +401,18 @@ router.post(
         $sort: {
           _id: 1,
         },
-      },
-      {
-        $unwind: '$data',
-      },
+      }
     ];
 
     if (query === 'min') {
-      console.log('inside min', metric);
-      pipeline.push(QueryList.min);
-      pipeline.push({
-        $sort: {
-          _id: 1,
-          "data,data." : 1,
-        },
-      });
+        pipeline = pipeline.concat(QueryList["min_pipeline"]);
     } else if (query === 'max') {
-      pipeline.push({
-        $sort: {
-          _id: 1,
-          'data.data.PM10': -1,
-        },
-      });
+        pipeline = pipeline.concat(QueryList["max_pipeline"]);
     } else if (query === 'avg') {
       console.log('inside avg');
       // pipeline.push(QueryList.avg);
-      pipeline.push({
-        $group: {
-          _id: '$_id',
-          avg: { $avg: '$data.data.PM10' },
-          data: { $first: '$$ROOT' },
-        },
-      });
-      pipeline.push({
-        $sort: {
-          _id: 1,
-        },
-      });
-      pipeline.push({
-        $project: {
-          _id: 1,
-          'data.recordedAt': '$data.data.recordedAt',
-          'data.metadata': '$data.data.metadata',
-          'data.data': '$data.data.data',
-          'data.uploadedBy': '$data.data.uploadedBy',
-          'data._id': '$data.data._id',
-          avg: '$avg',
-        },
-      });
-      pipeline.push({
-        $project: {
-          'data.data.PM10': 0,
-        },
-      });
-      pipeline.push({
-        $set: {
-          'data.data.PM10': '$avg',
-        },
-      });
-      pipeline.push({
-        $project: {
-          avg: 0,
-        },
-      });
+      pipeline = pipeline.concat(QueryList["avg_pipeline"]);
     }
-
-    pipeline.push({
-      $replaceRoot: {
-        newRoot: '$data',
-      },
-    });
-    pipeline.push({
-      $project: {
-        _id: 0,
-        id: '$_id',
-        timestamp: 1,
-        data: 1,
-        metadata: 1,
-        recordedAt: 1,
-        uploadedBy: 1,
-      },
-    });
 
     const pollutionData = await PollutionData.aggregate(pipeline);
 
